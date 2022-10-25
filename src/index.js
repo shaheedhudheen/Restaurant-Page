a428d3cab7fbb76cf5d03126aa808a63f3cb7e96
Ahammed Shaheedhudheen <shaheedhudheenpambalath@gmail.com>
2022-10-26 00:17:04 +0530

    working contact page

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import menuPage from "./modules/menu.js";
 import contactPage from "./modules/contact.js";
 import "./style.css";
 
-let header = document.querySelector('header')
-header.appendChild(navbar())
+let header = document.querySelector("header");
+header.appendChild(navbar());
 
 let content = document.querySelector("#content");
 const navBtn = document.querySelectorAll(".nav-link");
@@ -13,20 +13,23 @@ const navBtn = document.querySelectorAll(".nav-link");
 //tab swtitch function
 
 (function () {
-    content.appendChild(homepage())
-    let home = document.querySelector(".home")
-    home.addEventListener("click", ()=>{
-        content.replaceChildren(homepage())
-        
-    })
-    let menu = document.querySelector(".menu")
-    menu.addEventListener("click", ()=>{
-        
-        content.replaceChildren(menuPage());
-    })
-    let contact = document.querySelector(".contact")
-    contact.addEventListener("click", ()=>{
-        console.log("contact page");
-    })
-})();
+  content.appendChild(homepage());
 
+  let logo = document.querySelector(".nav-title");
+  logo.addEventListener("click", () => {
+    content.replaceChildren(homepage());
+  });
+
+  let home = document.querySelector(".home");
+  home.addEventListener("click", () => {
+    content.replaceChildren(homepage());
+  });
+  let menu = document.querySelector(".menu");
+  menu.addEventListener("click", () => {
+    content.replaceChildren(menuPage());
+  });
+  let contact = document.querySelector(".contact");
+  contact.addEventListener("click", () => {
+    content.replaceChildren(contactPage());
+  });
+})();
--- src/modules/contact.js
+++ src/modules/contact.js
@@ -1,5 +1,35 @@
 let contact = () => {
-  alert("contact");
+  const contact = document.createElement("div");
+  contact.classList.add("contactTab");
+
+  const contactHeading = document.createElement("h1");
+  contactHeading.textContent = "Find us here:";
+  contactHeading.classList.add("contact-heading");
+  contact.appendChild(contactHeading);
+
+  const days = document.createElement("p")
+  const time = document.createElement("p")
+  const place = document.createElement("p")
+  const email = document.createElement("p")
+
+  days.classList.add('contact-info')
+  time.classList.add('contact-info')
+  place.classList.add('contact-info')
+  email.classList.add('contact-info')
+
+  days.textContent = "Monday - Sunday";
+  time.textContent = "10am - 11pm";
+  place.textContent = "Pang, Malappuram";
+  email.textContent = "thefoodhouse@company.com";
+
+  contact.appendChild(days)
+  contact.appendChild(time)
+  contact.appendChild(place)
+  contact.appendChild(email)
+
+  return contact;
 };
 
 export default contact;
+
+
